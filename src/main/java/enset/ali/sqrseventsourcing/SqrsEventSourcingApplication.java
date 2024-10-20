938a015a4905fdce22dabdc68c077935449079a3
Ali Ben-Hammad <Ali.Ben-Hammad@pipenile.com>
2024-10-20 09:32:36 +0100

    Initial commit

---
+++ src/main/java/enset/ali/sqrseventsourcing/SqrsEventSourcingApplication.java
@@ -0,0 +1,13 @@
+package enset.ali.sqrseventsourcing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqrsEventSourcingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqrsEventSourcingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/enset/ali/sqrseventsourcing/SqrsEventSourcingApplicationTests.java
@@ -0,0 +1,13 @@
+package enset.ali.sqrseventsourcing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqrsEventSourcingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
