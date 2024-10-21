0eec8b84592f7d97bf8ebd7cf1aec2f5c583e9c6
Ali Ben-Hammad <Ali.Ben-Hammad@pipenile.com>
2024-10-21 11:30:59 +0100

    Add event store route to list events for an Account

--- src/main/java/enset/ali/sqrseventsourcing/commands/controllers/AccountCommandController.java
+++ src/main/java/enset/ali/sqrseventsourcing/commands/controllers/AccountCommandController.java
@@ -5,17 +5,20 @@ import enset.ali.sqrseventsourcing.commonApi.dtos.CreateAccountRequestDTO;
 import lombok.AllArgsConstructor;
 import org.axonframework.commandhandling.CommandBus;
 import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.eventsourcing.eventstore.EventStore;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("/commands/account")
 @AllArgsConstructor
 public class AccountCommandController {
     private CommandGateway commandGateway;
+    private EventStore eventStore;
     @PostMapping(path = "/create")
     public CompletableFuture<String> createAccount(@RequestBody CreateAccountRequestDTO createAccountDTO) {
         CompletableFuture<String> commandResponse = commandGateway.send(
@@ -33,7 +36,12 @@ public class AccountCommandController {
                 exception.getMessage(),
                 org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR
         );
-
         return entity;
     }
+
+    @GetMapping("/eventStore/{accountId}")
+    public Stream eventStore(@PathVariable String accountId) {
+        return eventStore.readEvents(accountId).asStream();
+    }
+
 }
